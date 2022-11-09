fd2ba068d473e9dc60482521bf9d5b9b4ac07d4a
annar <p4nd4@posteo.de>
2022-11-09 19:13:17 +0100

    handlebars on signers-page working, new signatories are written into the db

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
 //Pg = postgresql
+const { query } = require("express");
 const spicedPg = require("spiced-pg");
-const { USER, PWD } = process.env;
+const { POSTGRES_PWD, POSTGRES_USER } = process.env;
+// console.log(POSTGRES_PWD, POSTGRES_USER);
 const database = "petition";
-const db = spicedPg(`postgres:spicedling:${PWD}@localhost:5432/${database}`);
+//5432 = standardport
+const db = spicedPg(
+    `postgres:postgres:${POSTGRES_PWD}@localhost:5432/${database}`
+);
 
 //.querymethod to qurey my database
-db.query(`SELECT * FROM ${database}`)
+db.query(`SELECT * FROM signatures`)
     .then(function (result) {
         console.log(result.rows);
     })
@@ -14,7 +19,25 @@ db.query(`SELECT * FROM ${database}`)
     });
 
 //Preventing SQL injection https://spiced.space/okra/spiced_pg/
+module.exports.getSubscribers = () => {
+    return db.query(`SELECT firstname, lastname FROM signatures`);
+};
 
-// function getActorByName(actorName) {
-//     return db.query(`SELECT * FROM actors WHERE name = $1`, [actorName]);
+module.exports.insertSubscriber = ({ firstname, lastname, signature }) => {
+    return db
+        .query(
+            `INSERT INTO signatures (firstname, lastname, signature)
+            VALUES($1, $2, $3)
+            RETURNING *`,
+            //RETURNING gibt die spalten an, die zurueck gegeben werden im result
+            [firstname, lastname, signature]
+        ) //once data is saved, set cookie to remember this, then res.render("thanks");
+        .then((result) => {
+            // console.log("result", result);
+            result.rows[0];
+        });
+};
+// do we need to crate a account table?
+// module.exports.findUserByEmail(email) = () => {
+//     return db.query(`SELECT * FROM users WHERE email=$1`, [email]);
 // }
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
-//const db = require("db");
+const db = require("./db");
 const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const { getSignatureTable } = require("./db.js");
+const PORT = 8080;
+const { SECRET } = process.env;
 
+//handlebars
 const handlebars = require("express-handlebars");
 app.engine("handlebars", handlebars.engine());
 app.set("view engine", "handlebars");
@@ -20,45 +24,59 @@ const cookieSession = require("cookie-session");
 app.use(
     cookieSession({
         // secret is used to generate the 2. cookie used to verify the integrity of the 1. cookie
-        secret: `I'm always angry.`,
+        secret: `${SECRET}`,
         maxAge: 1000 * 60 * 60 * 24 * 14,
     })
 );
 
+//MIDDLEWARE
 app.use("/", express.static(path.join(__dirname, "public")));
+
+//ROUTES
 
 //petition-page
 app.get("/", (req, res) => {
-    console.log("get req");
     // check negative first so i dont need an else
     // if (!(req.session && req.session.user_id){return res.redirect(/login)})
     res.render("petition", {
-        title: "Petition",
+        title: "Snack Box Petition",
     });
 });
 
 app.post("/", (req, res) => {
-    console.log("post req");
-    res.setHeader("Content-Type", "text/html");
-    res.cookie("signed", "true");
-    res.statusCode = 200;
-    return res.redirect("/thanks");
+    //console.log("post req");
+    const { firstName, lastName, signatureCanvas } = req.body;
+    db.insertSubscriber({
+        firstname: firstName,
+        lastname: lastName,
+        //signatureCanvas: how to do this?
+    }).then(() => {
+        // res.cookie("signed", "true");
+        res.redirect("/thanks");
+    });
 });
 
 //thank-you-page
 app.get("/thanks", (req, res) => {
-    console.log("get tanks");
+    //kaputt
     res.render("thanks", {
-        title: "Petition",
+        title: "Snack Box Petition",
     });
+    db.getSubscribers()
+        .then((result) => {})
+        .catch((err) => console.log(err));
 });
 
 //signers page
 app.get("/signers", (req, res) => {
-    console.log("get tanks");
-    res.render("signers", {
-        title: "Petition",
+    //  console.log("get thanks");
+    db.getSubscribers().then((results) => {
+        console.log(results);
+        res.render("signers", {
+            title: "Snack Box Petition",
+            fullname: results.rows,
+        });
     });
 });
 
-app.listen(8080, () => console.log("server listening to 8080"));
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
